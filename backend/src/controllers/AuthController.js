f6a63d2ee31766ba7a90eae30e0c5bb865497ef0
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-08-21 00:18:26 -0400

    feat: auth create

---
+++ backend/src/controllers/AuthController.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class AuthController {
+  async create(req, res) {
+    try {
+      const { email = '', password = '' } = req.body;
+
+      if (!email || !password) {
+        return res.status(401).json({ errors: ['Dados inválidos'] });
+      }
+
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return res.status(401).json({ errors: ['Dados inválidos'] });
+      }
+
+      if (!(await user.passwordIsValid(password))) {
+        return res.status(401).json({ errors: ['Dados inválidos'] });
+      }
+
+      const { id } = user;
+      const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+        expiresIn: process.env.TOKEN_EXPIRATION,
+      });
+
+      return res.status(200).json({ token });
+    } catch (e) {
+      return res.status(400).json(
+        { errors: ['Dados inválidos'] },
+      );
+    }
+  }
+}
+
+export default new AuthController();
