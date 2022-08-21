75ad044bfe1bfe76a97eee01454d07fa6073d768
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-08-20 21:05:56 -0400

    chore: config database and sequelize

---
+++ backend/src/config/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Manaus',
+  },
+  timezone: 'America/Manaus',
+};
